e58320e7ddc1bd0bcfb7c09c35c5c92874898509
Nicholas Serrano <34073804+nickroxcks@users.noreply.github.com>
2019-12-05 02:01:43 -0700

    added read command

--- FileSystem.h
+++ FileSystem.h
@@ -16,7 +16,7 @@ typedef struct {
 void fs_mount(char *new_disk_name);
 void fs_create(std::string name, int size);
 void fs_delete(std::string name);
-void fs_read(char name[5], int block_num);
+void fs_read(std::string name, int block_num);
 void fs_write(char name[5], int block_num);
 void fs_buff(uint8_t buff[1024]);
 void fs_ls(void);
